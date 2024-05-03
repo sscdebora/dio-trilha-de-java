cc51d267f64003a681a12493e469be206b78399e
Debora Cristina <deboracristinasantos5@gmail.com>
2024-05-02 23:24:12 -0300

    projeto de conta bancaria

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.println("Entre com o numero da conta: ");
+        int numeroAgencia = scanner.nextInt();
+        
+
+        System.out.println("Entre com o numero da agÊncia: ");
+        String numeroConta = scanner.nextLine();
+        scanner.nextLine();
+
+        System.out.println("Entre com o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Entre com o saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá, " + nomeCliente + "Sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
+
+
+
+
+
+
     }
 }
